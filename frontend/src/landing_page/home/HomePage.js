61c49c96f450713e0b40c4889f690e10641f8e3f
alfiyainamdar31 <alfiyainamdar143@gmail.com>
2024-07-14 22:24:27 +0530

    home page

--- frontend/src/landing_page/home/HomePage.js
+++ frontend/src/landing_page/home/HomePage.js
@@ -1,27 +1,27 @@
-import React from 'react';
-import Hero from './Hero';
-import Awards from './Awards';
-import Pricing from './Pricing';
-import Education from './Education';
-import Stats from './Stats';
+import React from "react";
+import Hero from "./Hero";
+import Awards from "./Awards";
+import Pricing from "./Pricing";
+import Education from "./Education";
+import Stats from "./Stats";
 
-import OpenAccount from '../OpenAccount';
-import Navbar from '../Navbar';
-import Footer from '../Footer';
+import OpenAccount from "../OpenAccount";
+import Navbar from "../Navbar";
+import Footer from "../Footer";
 
 function HomePage() {
-    return ( 
-        <>
-           <Navbar />
-           <Hero />
-           <Awards />
-           <Stats />
-           <Pricing />
-           <Education />
-           <OpenAccount />
-           <Footer />
-        </>
-    );
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Awards />
+      <Stats />
+      <Pricing />
+      <Education />
+      <OpenAccount />
+      <Footer />
+    </>
+  );
 }
 
 export default HomePage;
