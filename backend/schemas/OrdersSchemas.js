cd9e68b52cb57ad6ab7155746635231ec02e0386
alfiyainamdar31 <alfiyainamdar143@gmail.com>
2024-07-23 15:19:20 +0530

    schemas and models setup

---
+++ backend/schemas/OrdersSchemas.js
@@ -0,0 +1,10 @@
+const { Schema } = require("mongoose");
+
+const OrdersSchema = new Schema({
+  name: String,
+  qty: Number,
+  Price: Number,
+  mode: String,
+});
+
+module.exports = { OrdersSchema };
